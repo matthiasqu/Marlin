6b5b1b5918e11f963ac98b632731fabff367e466
Matthias Quaß <matthias.quass@uni-due.de>
2020-09-25 14:34:33 +0200

    Updated PID settings (hotend)

--- config/Configuration.h
+++ config/Configuration.h
@@ -490,9 +490,9 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
   // Creality Ender-3
-  #define DEFAULT_Kp 21.73
-  #define DEFAULT_Ki 1.54
-  #define DEFAULT_Kd 76.55
+  #define DEFAULT_Kp 8.15
+  #define DEFAULT_Ki 1.26
+  #define DEFAULT_Kd 13.18
 
   // Ultimaker
   //#define DEFAULT_Kp 22.2
